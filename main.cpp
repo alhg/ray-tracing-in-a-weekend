3a48ecb394b04590754a0c11bdc84902fd7dcefe
alhg <alhg@users.noreply.github.com>
2022-02-05 13:42:05 -0800

    Formatting with VSCode C++ formatter

--- main.cpp
+++ main.cpp
@@ -4,13 +4,15 @@
 
 #include <iostream>
 
-color ray_color(const ray& r) {
+color ray_color(const ray &r)
+{
     vec3 unit_direction = unit_vector(r.direction());
-    double t = 0.5*(unit_direction.y() + 1.0);
-    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+    double t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
-int main() {
+int main()
+{
     // Image
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
@@ -24,21 +26,23 @@ int main() {
     auto origin = point3(0, 0, 0);
     auto horizontal = vec3(viewport_width, 0, 0);
     auto vertical = vec3(0, viewport_height, 0);
-    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
 
     // Render
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    std::cout << "P3\n"
+              << image_width << ' ' << image_height << "\n255\n";
 
-    for (int j = image_height - 1; j >= 0; --j) {
-	std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-	for (int i = 0; i < image_width; ++i) {
-	    auto u = double(i) / (image_width-1);
-	    auto v = double(j) / (image_height-1);
-	    ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
-	    color pixel_color = ray_color(r);
-	    write_color(std::cout, pixel_color);
-	}
+    for (int j = image_height - 1; j >= 0; --j)
+    {
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        for (int i = 0; i < image_width; ++i)
+        {
+            auto u = double(i) / (image_width - 1);
+            auto v = double(j) / (image_height - 1);
+            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+            color pixel_color = ray_color(r);
+            write_color(std::cout, pixel_color);
+        }
     }
-
     std::cerr << "\nDone.\n";
 }
